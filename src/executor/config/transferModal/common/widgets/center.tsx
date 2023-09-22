2da98082a66af8e261ea0898cb9e05f4b8c25e0e
15168347908 <296778233@qq.com>
2023-09-22 15:48:14 +0800

    fix: 打开表格

--- src/executor/config/transferModal/common/widgets/center.tsx
+++ src/executor/config/transferModal/common/widgets/center.tsx
@@ -2,6 +2,8 @@ import { ITransfer } from '@/ts/core';
 import React, { ReactNode, useEffect, useState } from 'react';
 import { model } from '@/ts/base';
 import { MappingModal, RequestModal, StoreModal, TransferModal } from '../..';
+import OfficeView from '@/executor/data/open/office';
+import { message } from 'antd';
 
 interface IProps {
   current: ITransfer;
@@ -9,6 +11,7 @@ interface IProps {
 
 const Editable: React.FC<IProps> = ({ current }) => {
   const [center, setCenter] = useState<ReactNode>();
+  const setEmpty = () => setCenter(<></>);
   useEffect(() => {
     const id = current.command.subscribe(async (type, cmd, args) => {
       if (type != 'tools') return;
@@ -17,45 +20,38 @@ const Editable: React.FC<IProps> = ({ current }) => {
           switch (args.typeName) {
             case '请求':
               setCenter(
-                <RequestModal
-                  finished={() => setCenter(<></>)}
-                  transfer={current}
-                  current={args}
-                />,
+                <RequestModal finished={setEmpty} transfer={current} current={args} />,
               );
               break;
             case '映射':
               setCenter(
-                <MappingModal
-                  finished={() => setCenter(<></>)}
-                  transfer={current}
-                  current={args}
-                />,
+                <MappingModal finished={setEmpty} transfer={current} current={args} />,
               );
               break;
             case '存储':
               setCenter(
-                <StoreModal
-                  finished={() => setCenter(<></>)}
-                  transfer={current}
-                  current={args}
-                />,
+                <StoreModal finished={setEmpty} transfer={current} current={args} />,
               );
               break;
             case '子图':
-              const node = args as model.SubTransfer;
-              const nextId = node.nextId;
+              const subTransfer = args as model.SubTransfer;
+              const nextId = subTransfer.nextId;
               const nextTransfer = current.getTransfer(nextId);
               setCenter(
                 <>
                   {nextTransfer && (
-                    <TransferModal
-                      current={nextTransfer}
-                      finished={() => setCenter(<></>)}
-                    />
+                    <TransferModal current={nextTransfer} finished={setEmpty} />
                   )}
                 </>,
               );
+              break;
+            case '表格':
+              const tables = args as model.Tables;
+              if (tables.file) {
+                setCenter(<OfficeView share={tables.file} finished={setEmpty} />);
+              } else {
+                message.error('未上传文件');
+              }
               break;
           }
           break;
